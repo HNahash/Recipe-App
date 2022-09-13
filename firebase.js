df2182cd3aacfe9324ec9707ae73e9e0a420dc15
Hussain Al Nahash <hnahash@gmail.com>
2022-09-13 21:11:04 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,14 +7,8 @@ import "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCNg_vksVCFju5PoXe881SYmrIPdEmqOPs",
-  authDomain: "recipe-491b3.firebaseapp.com",
-  projectId: "recipe-491b3",
-  storageBucket: "recipe-491b3.appspot.com",
-  messagingSenderId: "179196063765",
-  appId: "1:179196063765:web:eea0ae8d4cad3bc7145e1c",
-};
+
+
 
 // Initialize Firebase
 let app;
